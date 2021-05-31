108c1bf19a6ba80c372805d31b56d679d5fa099c
yashith <yashith98@gmail.com>
2021-05-31 21:49:57 +0530

    Formik validation

--- Thinksite/src/Components/signup.js
+++ Thinksite/src/Components/signup.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import { Container, Col, Row, Card, Form, Button } from 'react-bootstrap'
 import './signupstyle.css'
-function Signup() {
+import { useFormik } from 'formik'
 
+const validate = values => {
+    const errors = {};
+    if (!values.name) {
+        errors.name = 'Required';
+    }
+    if (!values.username) {
+        errors.username = 'Required';
+    }
+    if (!values.password) {
+        errors.password = 'Required';
+    }
+    else if (values.password.length <= 8) {
+        errors.password = 'Password must be more then 8 characters'
+    }
+    if (!values.institute) {
+        errors.institute = 'Required';
+    }
+    if (!values.email) {
+        errors.email = 'Required';
+    }
+    if (!values.category) {
+        errors.category = 'Required';
+    }
+
+    return(errors)
+}
+function Signup() {
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            name: '',
+            password: '',
+            institute: '',
+            email: '',
+            category: '',
+        },
+        validate,
+        onSubmit: values => {
+            console.log(JSON.stringify(values))
+        }
+    })
     return (
         <>
             <Container className="maindiv">
@@ -13,22 +54,39 @@ function Signup() {
                                 Signup
                     </Card.Header>
                             <Card.Body>
-                                <Form>
+                                <Form onSubmit={formik.handleSubmit}>
                                     <Form.Group >
                                         <Form.Label className='align-left'>UserName</Form.Label>
-                                        <Form.Control type="text" />
+                                        <Form.Control type="text" id='username' name='username' onChange={formik.handleChange} value={formik.values.username} />
+                                        {formik.errors.username?<small className="red"><span>{formik.errors.username}</span></small>:null}
                                     </Form.Group>
                                     <Form.Group>
                                         <Form.Label>Name</Form.Label>
-                                        <Form.Control type="text" />
+                                        <Form.Control type="text" id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
+                                        {formik.errors.name?<small className="red"><span>{formik.errors.name}</span></small>:null}
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" aria-describedby="emailHelp" id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
+                                        {formik.errors.email?<small className="red"><span>{formik.errors.email}</span></small>:null}
                                     </Form.Group>
                                     <Form.Group>
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" />
+                                        <Form.Control type="password" id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
+                                        {formik.errors.password?<small className="red"><span>{formik.errors.password}</span></small>:null}
                                     </Form.Group>
                                     <Form.Group>
-                                        <Form.Label>Re-Enter Password</Form.Label>
-                                        <Form.Control type="password" />
+                                        <Form.Label>Institute</Form.Label>
+                                        <Form.Control type="text" id='institute' name='institute' onChange={formik.handleChange} value={formik.values.institute} />
+                                        {formik.errors.institute?<small className="red"><span>{formik.errors.institute}</span></small>:null}
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label>Category</Form.Label>
+                                        <Form.Control as="select" id='category' name='category' onChange={formik.handleChange} value={formik.values.category} >
+                                            <option value="School">School</option>
+                                            <option value="University">University</option>
+                                        </Form.Control>
+                                        {formik.errors.category?<small className="red"><span>{formik.errors.category}</span></small>:null}
                                     </Form.Group>
                                     <div className="btnsub">
                                         <Button type='submit' variant='success'>Signup</Button>
